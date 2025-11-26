1793a0332e48b5dd2aace3d4a4a2b9debf553ff2
Erick141ds <demiscarreon00@gmail.com>
2025-11-26 12:13:19 -0600

    Actualizacion del proyecto

--- djangostore/urls.py
+++ djangostore/urls.py
@@ -1,9 +1,10 @@
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    # Aquí tus rutas
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
